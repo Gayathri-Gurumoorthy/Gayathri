2d13265c0f78f0d7a10cbd603e920994a381b21b
Ananth M <129041374+ananth-ms@users.noreply.github.com>
2024-04-29 08:42:50 -0700

    Update javapro.java

--- src/main/java/javapro.java
+++ src/main/java/javapro.java
@@ -4,6 +4,7 @@ public static int setmini(int[] A, int N)
     int mini = Integer.MAX_VALUE;
     for (int i = 0; i < N; i++) {
         if (A[i] < mini) {
+            
             mini = A[i];
         }
     }
